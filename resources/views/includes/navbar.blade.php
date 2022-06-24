a72e00f7ac93e999b7888f33b7172f050e517564
alex-killer <onefrank@bk.ru>
2022-06-24 16:32:27 +0700

    1

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -3,29 +3,23 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/">Главная</a>
+                    <a class="btn btn-light" aria-current="page" href="/">Главная</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('wallet.index') }}">Кошелек</a>
+                    <a class="btn btn-light" href="{{ route('wallet.index') }}">Кошелек</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('transfer.index') }}">Переводы</a>
+                    <a class="btn btn-light" href="{{ route('transfer.index') }}">Переводы</a>
                 </li>
                 @if (Auth::check())
                 <li class="nav-item">
-                    <a class="nav-link" href="#">{{ Auth::user()->name }}</a>
+                    <a class="btn btn-light" href="#">{{ Auth::user()->name }}</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('logout') }}">
-                        {{ __('Выйти') }}
-                    </a>
                     <form action="{{ route('logout') }}" method="POST">
                         @csrf
+                        <input class="btn btn-light" type="submit" value="Выйти">
                     </form>
-{{--                    <form action="{{ route('logout') }}" method="POST">--}}
-{{--                        @csrf--}}
-{{--                        <input class="btn btn-light" type="submit" value="Выйти">--}}
-{{--                    </form>--}}
                 </li>
                 @endif
             </ul>
